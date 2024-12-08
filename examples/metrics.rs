36c849c735ef102982d395fa2b96c5f40665cd24
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-12-08 17:11:36 +0800

    feature: support rw lock & display

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -9,47 +9,47 @@ const M: usize = 4;
 
 fn main() -> Result<()> {
     let metrics = Metrics::new();
-    // for i in 0..100 {
-    //     metrics.inc("req.page.1");
-    //     metrics.inc("req.page.2");
-    //     if i % 2 == 0 {
-    //         metrics.inc("req.page.3");
-    //     }
-    // }
-    // for _ in 0..27 {
-    //     metrics.inc("call.thread.worker.1");
-    // }
 
     println!("metrics: {:?}", metrics.snapshot());
 
     for idx in 0..N {
-        task_worker(idx, metrics.clone()); // Metrics {data: Arc::clone(&metrics.data)}
+        task_worker(idx, metrics.clone())?; // Metrics {data: Arc::clone(&metrics.data)}
     }
 
     for _ in 0..M {
-        request_worker(metrics.clone());
+        request_worker(metrics.clone())?;
     }
     loop {
         thread::sleep(Duration::from_secs(2));
-        println!("metrics: {:?}", metrics.snapshot());
+        println!("metrics: {}", metrics);
     }
 }
 
-fn task_worker(idx: usize, metrics: Metrics) {
-    thread::spawn(move || loop {
-        // do long term stuff
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics.inc(format!("call.thread.worker.{}", idx)).unwrap();
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            // do long term stuff
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
 
-fn request_worker(metrics: Metrics) {
-    thread::spawn(move || loop {
-        // do long term stuff
-        let mut rng = rand::thread_rng();
-        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page = rng.gen_range(1..256);
-        metrics.inc(format!("req.page.{}", page)).unwrap();
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            // do long term stuff
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..256);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
--- src/metrics.rs
+++ src/metrics.rs
@@ -4,36 +4,47 @@
 use anyhow::{anyhow, Result};
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    fmt::{self, Display},
+    sync::{Arc, RwLock},
 };
 #[derive(Debug, Clone, Default)]
 pub struct Metrics {
-    data: Arc<Mutex<HashMap<String, i64>>>,
+    data: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: Arc::new(Mutex::new(HashMap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let count = data.entry(key.into()).or_insert(0);
         *count += 1;
         Ok(())
     }
 
     pub fn dec(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?;
         let count = data.entry(key.into()).or_insert(0);
         *count -= 1;
         Ok(())
     }
 
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        let data = self.data.lock().map_err(|e| anyhow!(e.to_string()))?;
+        let data = self.data.read().map_err(|e| anyhow!(e.to_string()))?;
         Ok(data.clone())
+    }
+}
+
+impl Display for Metrics {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let data = self.data.read().map_err(|_e| fmt::Error {})?;
+        for (k, v) in data.iter() {
+            writeln!(f, "{}: {}", k, v)?;
+        }
+        Ok(())
     }
 }
